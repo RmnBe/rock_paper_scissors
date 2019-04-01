4e88a1f48873f25dcd1011775826c92725b31ab5
Roman Bednarz <roman.bednarz@gmail.com>
2019-04-01 18:15:32 +0200

    done with corrections

--- js/script.js
+++ js/script.js
@@ -10,20 +10,20 @@ var roundsWindow = document.getElementById('rounds');
 var playerScore = 0;
 var computerScore = 0;
 var totalRounds = 0;
+var gameOver = false;
 
 newGameBtn.addEventListener('click', function(){
-	totalRounds = window.prompt('Podaj ilość rund do rozegrania');
+	totalRounds = window.prompt('Podaj ilość wygranych rund potrzebnych do zakończenia gry!');
 	roundsWindow.innerHTML = totalRounds;
 	outputWindow.innerHTML = "Let's play!";
 	resultWindow.innerHTML = "0:0";
 	computerScore = 0;
 	playerScore = 0;
-	console.log(totalRounds);
+	
 });
 
 rockBtn.addEventListener('click', function(){
 	playerMove('rock');
-	console.log(totalRounds);
 });
 paperBtn.addEventListener('click', function(){
 	playerMove('paper');
@@ -50,8 +50,72 @@ function playerMove(choice){
 	var compMove = computerMove();
 	console.log(playerMove);
 	console.log(compMove);
+	
+	if (gameOver == false){
+		if (playerMove =='rock' && compMove == 'rock') {
+		outputWindow.innerHTML = "It's a tie! You played Rock and computer also played Rock.";
+		}
+		else if (playerMove == 'rock' && compMove == 'paper') {
+			outputWindow.innerHTML = "You lose! Paper covers rock."
+			computerScore = computerScore + 1;
+		}
+		else if (playerMove == 'rock' && compMove == 'scissor') {
+			outputWindow.innerHTML = "You won! Rock crushes scissors."
+			playerScore = playerScore + 1;
+		}
+		else if (playerMove == 'paper' && compMove == 'rock') {
+			outputWindow.innerHTML = 'You won! Paper covers rock'
+			playerScore = playerScore + 1;
+		}
+		else if (playerMove == 'paper' && compMove == 'paper') {
+			outputWindow.innerHTML = "It's a tie! You played Paper and computer also played Paper."
+		}
+		else if (playerMove == 'paper' && compMove == 'scissor') {
+			outputWindow.innerHTML = 'You lose! Scissors cuts paper.'
+			computerScore = computerScore + 1;
+		}
+		else if (playerMove == 'scissor' && compMove == 'rock') {
+			outputWindow.innerHTML = 'You lose! Rock crushes scissors.'
+			computerScore = computerScore + 1;
+		}
+		else if (playerMove == 'scissor' && compMove == 'paper') {
+			outputWindow.innerHTML = 'You won! Scissors cuts paper.'
+			playerScore = playerScore + 1;
+		}
+		else if (playerMove == 'scissor' && compMove == 'scissor') {
+			outputWindow.innerHTML = "It's a tie! You played Scissor and computer also played Scissor."
+		}
+	}
+	else {
+		outputWindow.innerHTML = 'End of game! Press "New Game" button!'; 
+	}
+	refreshResults();
+	checkRounds();
+	console.log(playerScore);
+	console.log(computerScore);
+	console.log(totalRounds);
+}
+function refreshResults(){
+	resultWindow.innerHTML = playerScore +":"+ computerScore;
+}
+function checkRounds(){
+	if (totalRounds == playerScore || totalRounds == computerScore){
+		if (playerScore > computerScore){
+			outputWindow.innerHTML = outputWindow.innerHTML + "<br>" + " End of game. You won! " + playerScore + ":" + computerScore;
+			gameOver = true;
+		}
+		else if (playerScore < computerScore){
+			outputWindow.innerHTML = outputWindow.innerHTML + "<br>" + " End of game. You lose! " + playerScore + ":" + computerScore;
+			gameOver = true;
+		}
+}
+}
+	
 
-	if (totalRounds < 1) {
+
+
+
+	/*if (totalRounds < 1) {
 		outputWindow.innerHTML = "End of game!"; 
 	}
 	else if (totalRounds == 1 && playerScore == computerScore) {
@@ -65,56 +129,8 @@ function playerMove(choice){
 	else if (totalRounds == 1 && playerScore < computerScore) {
 		outputWindow.innerHTML = outputWindow.innerHTML + "<br>" + " End of game. You lose! " + playerScore + ":" + computerScore;
 		totalRounds = totalRounds - 1;
-	}
-	else if (playerMove =='rock' && compMove == 'rock') {
-		outputWindow.innerHTML = "It's a tie! You played Rock and computer also played Rock.";
-		totalRounds = totalRounds - 1;
-	}
-	else if (playerMove == 'rock' && compMove == 'paper') {
-		outputWindow.innerHTML = "You lose! Paper covers rock."
-		computerScore = computerScore + 1;
-		totalRounds = totalRounds - 1;
-	}
-	else if (playerMove == 'rock' && compMove == 'scissor') {
-		outputWindow.innerHTML = "You won! Rock crushes scissors."
-		playerScore = playerScore + 1;
-		totalRounds = totalRounds - 1;
-	}
-	else if (playerMove == 'paper' && compMove == 'rock') {
-		outputWindow.innerHTML = 'You won! Paper covers rock'
-		playerScore = playerScore + 1;
-		totalRounds = totalRounds - 1;
-	}
-	else if (playerMove == 'paper' && compMove == 'paper') {
-		outputWindow.innerHTML = "It's a tie! You played Paper and computer also played Paper."
-		totalRounds = totalRounds - 1;
-	}
-	else if (playerMove == 'paper' && compMove == 'scissor') {
-		outputWindow.innerHTML = 'You lose! Scissors cuts paper.'
-		computerScore = computerScore + 1;
-		totalRounds = totalRounds - 1;
-	}
-	else if (playerMove == 'scissor' && compMove == 'rock') {
-		outputWindow.innerHTML = 'You lose! Rock crushes scissors.'
-		computerScore = computerScore + 1;
-		totalRounds = totalRounds - 1;
-	}
-	else if (playerMove == 'scissor' && compMove == 'paper') {
-		outputWindow.innerHTML = 'You won! Scissors cuts paper.'
-		playerScore = playerScore + 1;
-		totalRounds = totalRounds - 1;
-	}
-	else if (playerMove == 'scissor' && compMove == 'scissor') {
-		outputWindow.innerHTML = "It's a tie! You played Scissor and computer also played Scissor."
-		totalRounds = totalRounds - 1;
-	}
-	refreshResults();
-}
-function refreshResults(){
-	if (totalRounds > 0) {
-		resultWindow.innerHTML = playerScore +":"+ computerScore;
-	}
-}
+	}*/
+
 
 
 
